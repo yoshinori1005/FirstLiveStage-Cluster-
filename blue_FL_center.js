f9ad2d1d4c6ceb35b1914f5037f9bb290807f8d6
yoshinori1005 <s-yoshinori@jcom.home.ne.jp>
2025-03-23 00:34:16 +0900

    不具合修正
    点滅状態から各位置だけの色変えが正常に動作していなかったのを修正

--- blue_FL_center.js
+++ blue_FL_center.js
@@ -8,11 +8,22 @@ const targets = [
     $.worldItemReference("SpotLightRight"),
     $.worldItemReference("SpotLightLeft"),
     $.worldItemReference("SearchLights_R"),
-    $.worldItemReference("SearchLights_L")
+    $.worldItemReference("SearchLights_L"),
+    $.worldItemReference("FloorLight_1"),
+    $.worldItemReference("FloorLight_4"),
+    $.worldItemReference("MiniFloorLight_1"),
+    $.worldItemReference("MiniFloorLight_2"),
+    $.worldItemReference("MiniFloorLight_5"),
+    $.worldItemReference("MiniFloorLight_6")
 ];
 
 $.onInteract(() => {
-    for (let i = 0; i < targets.length; i++) {
-        targets[i].send("BlueSignal", null);
+    const mainTargets = targets.slice(0,10 );
+    const subTargets = targets.slice(10, 16);
+    for (let i = 0; i < mainTargets.length; i++) {
+        mainTargets[i].send("BlueSignal", null);
+    }
+    for (let i = 0; i < subTargets.length; i++) {
+        subTargets[i].send("BaseSignal", null);
     }
 });
--- red_FL_left.js
+++ red_FL_left.js
@@ -1,11 +1,29 @@
-const targets = [
+let targets = [
     $.worldItemReference("FloorLight_4"),
     $.worldItemReference("MiniFloorLight_5"),
     $.worldItemReference("MiniFloorLight_6"),
+    $.worldItemReference("FloorLight_1"),
+    $.worldItemReference("FloorLight_2"),
+    $.worldItemReference("FloorLight_3"),
+    $.worldItemReference("MiniFloorLight_1"),
+    $.worldItemReference("MiniFloorLight_2"),
+    $.worldItemReference("MiniFloorLight_3"),
+    $.worldItemReference("MiniFloorLight_4"),
+    $.worldItemReference("SpotLightFront"),
+    $.worldItemReference("SpotLightBack"),
+    $.worldItemReference("SpotLightRight"),
+    $.worldItemReference("SpotLightLeft"),
+    $.worldItemReference("SearchLights_R"),
+    $.worldItemReference("SearchLights_L")
 ];
 
 $.onInteract(() => {
-    for (let i = 0; i < targets.length; i++) {
-        targets[i].send("RedSignal", null);
+    const mainTargets = targets.slice(0, 3);
+    const subTargets = targets.slice(3, 16);
+    for (let i = 0; i < mainTargets.length; i++) {
+        mainTargets[i].send("RedSignal", null);
+    }
+    for (let i = 0; i < subTargets.length; i++) {
+        subTargets[i].send("BaseSignal", null);
     }
 });
